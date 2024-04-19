5b79c039d63eed178a3212d6a2cf4fa9ff336546
Mitchel Disveld <mitchel.disveld@hotmail.com>
2024-04-19 20:00:19 +0200

    rtmp implementation

--- go.mod
+++ go.mod
@@ -1,18 +1,16 @@
 module gonovon
 
-go 1.20
+go 1.21
 
 require (
-	github.com/buger/jsonparser v1.1.1 // indirect
+	code.cloudfoundry.org/bytefmt v0.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/hashicorp/logutils v1.0.0 // indirect
 	github.com/hashicorp/yamux v0.0.0-20190923154419-df201c70410d // indirect
 	github.com/huin/goupnp v1.0.0 // indirect
 	github.com/jackpal/gateway v1.0.5 // indirect
 	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
 	github.com/klauspost/cpuid v1.2.3 // indirect
 	github.com/klauspost/reedsolomon v1.9.3 // indirect
-	github.com/mmcloughlin/profile v0.1.1 // indirect
 	github.com/nknorg/go-nat v1.0.1 // indirect
 	github.com/nknorg/go-portscanner v0.0.0-20181002101859-8493ef01db79 // indirect
 	github.com/nknorg/nnet v0.0.0-20220621093239-b22b80b04216 // indirect
@@ -26,30 +24,103 @@ require (
 	github.com/xtaci/smux v2.0.1+incompatible // indirect
 	gitlab.com/NebulousLabs/fastrand v0.0.0-20181126182046-603482d69e40 // indirect
 	gitlab.com/NebulousLabs/go-upnp v0.0.0-20181011194642-3a71999ed0d3 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
 
 require (
-	github.com/andreykaipov/goobs v1.2.3
-	github.com/fsnotify/fsnotify v1.7.0
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/itchyny/base58-go v0.0.5 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/nknorg/ncp-go v1.0.5 // indirect
 	github.com/nknorg/nkn v1.1.7-beta
 	github.com/nknorg/nkn-sdk-go v1.4.7
 	github.com/nknorg/nkn/v2 v2.2.0 // indirect
 	github.com/nknorg/nkngomobile v0.0.0-20220615081414-671ad1afdfa9
-	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pbnjay/memory v0.0.0-20190104145345-974d429e7ae4 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
+
+require github.com/bluenviron/mediamtx v1.7.0
+
+require (
+	github.com/MicahParks/jwkset v0.5.17 // indirect
+	github.com/MicahParks/keyfunc/v3 v3.3.2 // indirect
+	github.com/abema/go-mp4 v1.2.0 // indirect
+	github.com/alecthomas/kong v0.9.0 // indirect
+	github.com/asticode/go-astikit v0.30.0 // indirect
+	github.com/asticode/go-astits v1.13.0 // indirect
+	github.com/benburkert/openpgp v0.0.0-20160410205803-c2471f86866c // indirect
+	github.com/bluenviron/gohlslib v1.3.1 // indirect
+	github.com/bluenviron/gortsplib/v4 v4.8.1 // indirect
+	github.com/bluenviron/mediacommon v1.9.3 // indirect
+	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/datarhei/gosrt v0.6.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/matthewhartstonge/argon2 v1.0.0 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pion/datachannel v1.5.5 // indirect
+	github.com/pion/dtls/v2 v2.2.7 // indirect
+	github.com/pion/ice/v2 v2.3.11 // indirect
+	github.com/pion/interceptor v0.1.29 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.9 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.14 // indirect
+	github.com/pion/rtp v1.8.5 // indirect
+	github.com/pion/sctp v1.8.8 // indirect
+	github.com/pion/sdp/v3 v3.0.9 // indirect
+	github.com/pion/srtp/v2 v2.0.18 // indirect
+	github.com/pion/stun v0.6.1 // indirect
+	github.com/pion/transport/v2 v2.2.3 // indirect
+	github.com/pion/turn/v2 v2.1.3 // indirect
+	github.com/pion/webrtc/v3 v3.2.22 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/term v0.19.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace code.cloudfoundry.org/bytefmt => github.com/cloudfoundry/bytefmt v0.0.0-20211005130812-5bb3c17173e5
+
+replace github.com/pion/ice/v2 => github.com/aler9/ice/v2 v2.0.0-20231112223552-32d34dfcf3a1
+
+replace github.com/pion/webrtc/v3 => github.com/aler9/webrtc/v3 v3.0.0-20231112223655-e402ed2689c6
+
+replace github.com/bluenviron/mediacommon => ../mediacommon
+
+replace github.com/bluenviron/gohlslib => ../gohlslib
+
+replace github.com/bluenviron/mediamtx => ../mediamtx
--- main.go
+++ main.go
@@ -1,27 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
-	"image"
-	"image/jpeg"
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/andreykaipov/goobs"
-	"github.com/andreykaipov/goobs/api/requests/general"
-	"github.com/nfnt/resize"
-
-	"github.com/fsnotify/fsnotify"
 	"github.com/nknorg/nkn-sdk-go"
 
-	_ "image/png"
+	"github.com/bluenviron/mediamtx/core"
 )
 
 var client *nkn.MultiClient
@@ -42,68 +34,18 @@ var segmentSendConfig = &nkn.MessageConfig{
 	Unencrypted: true,
 	NoReply:     true,
 }
-var streamPath = ""
-
-const SCREENSHOT_HOTKEY string = "OBSBasic.Screenshot"
 
 func main() {
-	fmt.Println("Welcome to go-novon a golang client for OBS streaming to novon")
+	fmt.Println("Welcome to go-novon a golang client for RTMP streaming to novon")
 	fmt.Println("")
-	fmt.Println("Make sure you have websockets enabled in OBS under Tools -> WebSocket Server Settings")
-	fmt.Println("If you have authentication enabled take note of the server password this will be required")
-	fmt.Println("")
-	fmt.Println("Make sure that OBS is configured for outputting HLS recordings")
-	fmt.Println("In OBS go to Settings -> Output tab, set the Output Mode to Advanced")
-	fmt.Println("Proceed to the Recording tab")
-	fmt.Println("Recording Format: 'HLS (.m3u8 + ts)'")
-	fmt.Println("Video Encoder: your preferred choice for 'x264'")
-	fmt.Println("")
-	fmt.Println("Scroll down to the Encoder Settings and set the following configuration")
-	fmt.Println("Keyframe Interval:'1s'")
-	fmt.Println("")
-	fmt.Println("go-novon will automatically detect the recording path and start broadcasting to novon")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("")
+
+	checkFfmpegInstalled()
 
 	var err error
 	config, err = NewConfig("./config.json")
 	if err != nil {
 		panic(err)
 	}
-
-	obs, err := goobs.New("localhost:4455")
-	if err != nil {
-		for {
-			if err.Error() == "websocket: close 4009: Authentication failed." {
-				reader := bufio.NewReader(os.Stdin)
-				fmt.Print("OBS WebSocket password: ")
-				password, _ := reader.ReadString('\n')
-
-				fmt.Println(password)
-				obs, err = goobs.New("localhost:4455", goobs.WithPassword(strings.TrimSpace(password)))
-				if err != nil {
-					fmt.Println(err.Error())
-				} else {
-					break
-				}
-			} else {
-				fmt.Println("could not find a running OBS instance")
-				time.Sleep(time.Second)
-				obs, err = goobs.New("localhost:4455")
-				if err == nil {
-					break
-				}
-			}
-		}
-	}
-
-	defer obs.Disconnect()
-
-	directoryResponse, _ := obs.Config.GetRecordDirectory()
-	streamPath = directoryResponse.RecordDirectory
-
-	fmt.Println("Stream path found: ", streamPath)
 
 	client = createClient()
 	for i := 0; i < NUM_SUB_CLIENTS; i++ {
@@ -113,26 +55,21 @@ func main() {
 	fmt.Println("connected to NKN")
 	fmt.Println("Your address", client.Address())
 
-	recordStatus, _ := obs.Record.GetRecordStatus()
-	if !recordStatus.OutputActive {
-		obs.Record.StartRecord()
-		fmt.Println("OBS started recording")
-		defer obs.Record.StopRecord()
-	} else {
-		fmt.Println("OBS is recording")
-	}
-
 	viewers = NewViewers(30 * time.Second)
 	viewers.StartCleanup(time.Second)
 	defer viewers.Cleanup()
 
+	s, ok := core.New(os.Args[1:], publishTSPart)
+	if !ok {
+		os.Exit(1)
+	}
+	fmt.Println("RTMP server started")
+
 	announceStream()
 
 	receiveMessages(client, viewers)
-	takeScreenshot(obs)
 
-	//Attack()
-	dedup(streamPath)
+	s.Wait()
 }
 
 func Attack() {
@@ -140,39 +77,6 @@ func Attack() {
 	for i := 0; i < 1000; i++ {
 		rngAddr, _ := nkn.RandomBytes(32)
 		viewers.AddOrUpdateAddress(hex.EncodeToString(rngAddr))
-	}
-}
-
-func processFiles(event fsnotify.Event) {
-	if strings.HasSuffix(event.Name, ".png") {
-		resizeAndCacheScreenshot(event.Name)
-		return
-	}
-	if strings.HasSuffix(event.Name, ".ts") {
-		b, err := os.ReadFile(event.Name)
-		if err != nil {
-			// panic(err)
-			fmt.Println(err)
-			return
-		}
-
-		//Segment the data to max CHUNK_SIZE chunks
-		chunks := ChunkByByteSizeWithMetadata(b, CHUNK_SIZE, segmentId)
-		segmentId++
-
-		fmt.Println("Broadcasting video segment to: ", len(viewerAddresses), "viewers")
-
-		if len(viewerAddresses) > 0 {
-			for _, chunk := range chunks {
-				publish(chunk)
-			}
-		}
-		lastSegment = chunks
-
-		err = os.Remove(event.Name)
-		if err != nil {
-			fmt.Println(err)
-		}
 	}
 }
 
@@ -204,7 +108,7 @@ func receiveMessages(client *nkn.MultiClient, viewers *Viewers) {
 				//Send last segment to newly joined
 				if isNew {
 					for _, chunk := range lastSegment {
-						sendToClient(msg.Src, chunk)
+						go sendToClient(msg.Src, chunk)
 					}
 				}
 			} else if len(msg.Data) == 9 && string(msg.Data[:]) == "thumbnail" {
@@ -226,43 +130,6 @@ func receiveMessages(client *nkn.MultiClient, viewers *Viewers) {
 			}
 
 		}
-	}()
-}
-
-func takeScreenshot(obsClient *goobs.Client) {
-	go func() {
-		for {
-			screenshotHotkeyName := "OBSBasic.Screenshot"
-			obsClient.General.TriggerHotkeyByName(&general.TriggerHotkeyByNameParams{
-				HotkeyName: &screenshotHotkeyName,
-			})
-			time.Sleep(30 * time.Second)
-		}
-	}()
-}
-
-func resizeAndCacheScreenshot(path string) {
-	go func() {
-		imgBytes, err := os.ReadFile(path)
-		if err != nil {
-			return
-		}
-		image, _, err := image.Decode(bytes.NewReader(imgBytes))
-		if err != nil {
-			return
-		}
-		newImage := resize.Resize(256, 144, image, resize.NearestNeighbor)
-
-		if newImage == nil {
-			return
-		}
-
-		newImageBytes := new(bytes.Buffer)
-		jpeg.Encode(newImageBytes, newImage, nil)
-
-		os.Remove(path)
-
-		thumbnail = newImageBytes.Bytes()
 	}()
 }
 
@@ -302,4 +169,79 @@ func ChunkByByteSizeWithMetadata(data []byte, chunkSize int, segmentId int) [][]
 	}
 
 	return chunks
+}
+
+func publishTSPart(segment []byte) {
+	log.Println("SHOW TIME", len(segment))
+	//Segment the data to max CHUNK_SIZE chunks
+	chunks := ChunkByByteSizeWithMetadata(segment, CHUNK_SIZE, segmentId)
+	segmentId++
+
+	fmt.Println("Broadcasting -", "viewers:", len(viewerAddresses), "size:", len(segment), "chunks:", len(chunks))
+
+	if len(viewerAddresses) > 0 {
+		for _, chunk := range chunks {
+			publish(chunk)
+		}
+	}
+
+	if (segmentId-1)%10 == 0 {
+		go screengrabSegment(segment)
+	}
+
+	lastSegment = chunks
+}
+
+func screengrabSegment(segment []byte) {
+	// Output image file
+	outputFile := "screenshot.jpg"
+	width := "256"
+	height := "144"
+
+	// Command arguments for ffmpeg
+	cmd := exec.Command("ffmpeg",
+		"-i", "-", // read from stdin (pipe)
+		"-vframes", "1",
+		"-y",                                             // overwrite output file
+		"-vf", fmt.Sprintf("scale=%s:%s", width, height), // resize filter
+		outputFile)
+
+	var stdinPipe, stderrPipe bytes.Buffer
+	cmd.Stdin = &stdinPipe
+	cmd.Stderr = &stderrPipe
+
+	// Write MPEG-TS data to stdin pipe
+	stdinPipe.Write(segment)
+
+	err := cmd.Run()
+
+	if err != nil {
+		fmt.Println("Error capturing screenshot:", err)
+		fmt.Println("FFmpeg stderr:", stderrPipe.String())
+		return
+	}
+
+	// Read the temporary image file into memory
+	thumbnail, err = os.ReadFile(outputFile)
+	if err != nil {
+		fmt.Println("Error reading temporary image file:", err)
+		return
+	}
+
+	fmt.Println("Screenshot captured successfully:", outputFile)
+}
+
+func checkFfmpegInstalled() {
+	// Command to check for ffmpeg (replace with actual command if needed)
+	cmd := exec.Command("ffmpeg", "-version")
+
+	err := cmd.Run()
+	if err != nil {
+		// Handle ffmpeg not found error
+		fmt.Println("Error: ffmpeg is not installed. Please install ffmpeg and try again.")
+		return
+	}
+
+	// ffmpeg is available, continue with your application logic
+	fmt.Println("ffmpeg is installed. Proceeding...")
 }
